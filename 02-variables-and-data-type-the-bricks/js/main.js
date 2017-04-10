4e3eb46cba3c0eaea5ebcfc9f8b350b30ae1bf50
Abhishek Bhavalkar <Abhishek Bhavalkar>
2017-04-10 15:00:13 +0530

    added the second chapter

---
+++ 02-variables-and-data-type-the-bricks/js/main.js
@@ -0,0 +1,9 @@
+function announceTheArrival(){
+	alert("Aye aye! I'm coming from an external JS file.");	
+}
+function dynamicTxt(){
+	document.getElementById('dynamic').innerHTML = "Hello ladies, I am a dynamic text. Respect!"
+}
+function generateError(){
+	alert(errorVal);
+}
